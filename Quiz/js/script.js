54bb641fb4d0e9dd3ddffafa92f2ad9099002988
ValeriTodorov123 <valerytodorov5@gmail.com>
2022-10-01 20:41:34 +0300

    design issues solved

--- Quiz/js/script.js
+++ Quiz/js/script.js
@@ -1,36 +1,18 @@
 //selecting all required elements
 const start_btn = document.querySelector(".start_btn button");
-const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
-// const time_line = document.querySelector("header .time_line");
-// const timeText = document.querySelector(".timer .time_left_txt");
-// const timeCount = document.querySelector(".timer .timer_sec");
-
-// if startQuiz button clicked
-start_btn.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show info box
-}
-
-// if exitQuiz button clicked
-exit_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-}
 
 // if continueQuiz button clicked
-continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
+start_btn.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); //show quiz box
+    start_btn.remove();
+    showQuetions(que_count); //calling showQestions function
+    queCounter(que_numb); //passing que_numb value to queCounter
     showQuetions(0); //calling showQestions function
-    // queCounter(1); //passing 1 parameter to queCounter
-    // startTimer(3); //calling startTimer function
-    // startTimerLine(0); //calling startTimerLine function
 }
 
-//let timeValue =  3; // bio
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
@@ -54,9 +36,6 @@ restart_quiz.onclick = ()=>{
     queCounter(que_numb); //passing que_numb value to queCounter
     clearInterval(counter); //clear counter
     clearInterval(counterLine); //clear counterLine
-    // startTimer(timeValue); //calling startTimer function
-    // startTimerLine(widthValue); //calling startTimerLine function
-    // timeText.textContent = "Time Left"; //change the text of timeText to Time Left
     next_btn.classList.remove("show"); //hide the next button
 }
 
@@ -77,9 +56,6 @@ next_btn.onclick = ()=>{
         queCounter(que_numb); //passing que_numb value to queCounter
         clearInterval(counter); //clear counter
         clearInterval(counterLine); //clear counterLine
-        // startTimer(timeValue); //calling startTimer function
-        // startTimerLine(widthValue); //calling startTimerLine function
-        // timeText.textContent = "Time Left"; //change the timeText to Time Left
         next_btn.classList.remove("show"); //hide the next button
     }else{
         clearInterval(counter); //clear counter
@@ -146,64 +122,13 @@ function optionSelected(answer){
 }
 
 function showResult(){
-    info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 3
-        //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
-    }
-    else if(userScore > 1){ // if user scored more than 1
-        let scoreTag = '<span>and nice 😎, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
-    else{ // if user scored less than 1
-        let scoreTag = '<span>and sorry 😐, You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
+    let scoreTag = '<span>You got <p>&nbsp' + userScore +'&nbsp</p> out of <p>&nbsp'+ questions.length +'</p></span>';
+    scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+
 }
-
-// function startTimer(time){
-//     counter = setInterval(timer, 1000);
-//     function timer(){
-//         timeCount.textContent = time; //changing the value of timeCount with time value
-//         time--; //decrement the time value
-//         if(time < 9){ //if timer is less than 9
-//             let addZero = timeCount.textContent; 
-//             timeCount.textContent = "0" + addZero; //add a 0 before time value
-//         }
-//         if(time < 0){ //if timer is less than 0
-//             clearInterval(counter); //clear counter
-//             timeText.textContent = "Time Off"; //change the time text to time off
-//             const allOptions = option_list.children.length; //getting all option items
-//             let correcAns = questions[que_count].answer; //getting correct answer from array
-//             for(i=0; i < allOptions; i++){
-//                 if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
-//                     option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-//                     option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-//                     console.log("Time Off: Auto selected correct answer.");
-//                 }
-//             }
-//             for(i=0; i < allOptions; i++){
-//                 option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
-//             }
-//             next_btn.classList.add("show"); //show the next button if user selected any option
-//         }
-//     }
-// }
-
-// function startTimerLine(time){
-//     counterLine = setInterval(timer, 29);
-//     function timer(){
-//         time += 1; //upgrading time value with 1
-//         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-//         if(time > 549){ //if time value is greater than 549
-//             clearInterval(counterLine); //clear counterLine
-//         }
-//     }
-// }
 
 function queCounter(index){
     //creating a new span tag and passing the question number and total question
